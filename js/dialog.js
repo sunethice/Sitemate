74f64375b3fafea382ebd1e5b5422d1915db3f7a
Suneth Perera <sunethjoes@gmail.com>
2021-01-06 22:48:28 +1100

    remove unwanted code

--- js/dialog.js
+++ js/dialog.js
@@ -1,14 +1,10 @@
 export function Dialog(btnElement, msgVal, yesCallback, noCallback) {
-  //   const btn = btnElement.querySelectorAll(".star");
   btnElement.addEventListener("click", function (event) {
     showDialog(msgVal);
   });
 
   function showDialog(message) {
-    var messageDiv = document.getElementById("message");
-    messageDiv.innerHTML = "";
-    var result = confirm(message); //"Are you sure?");
-
+    var result = confirm(message);
     if (result) {
       yesCallback();
     } else {
